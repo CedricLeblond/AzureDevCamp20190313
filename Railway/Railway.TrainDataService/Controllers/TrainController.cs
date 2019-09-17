a0c2e250732a33f751951d9f42b1327485df3f74
Cedric Leblond <c.leblond@libertysurf.fr>
2019-09-17 18:18:03 +0000

    change namesapce to Railway

--- Railway/Railway.TrainDataService/Controllers/TrainController.cs
+++ Railway/Railway.TrainDataService/Controllers/TrainController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using HassanCehef.TrainDataService.Models;
+using Railway.TrainDataService.Models;
 
-namespace HassanCehef.TrainDataService.Controllers
+namespace Railway.TrainDataService.Controllers
 {
     [Route("api/data_for_train")]
     public class TrainController : Controller
